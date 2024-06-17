d28b8fee16a8d6d44da8e9fc40e8d8a9bae90c34
Isak Nordgren <isaknordgren@hotmail.se>
2024-06-17 12:59:30 +0200

    Added Swedish Beagle. Not tested

--- summarize_file.py
+++ summarize_file.py
@@ -7,12 +7,19 @@ import io
 
 import os
 from dotenv import load_dotenv
+
+# groq
 from groq import Groq
 
+# SwedishBeagle-dare
+from transformers import AutoTokenizer
+import transformers
+import torch
 
 class Summarizer:
 
-    def __init__(self):
+    def __init__(self, model = "groq"):
+        self.model = model
         self.client = self.load_groq()
 
     def run_app(self):
@@ -58,6 +65,17 @@ class Summarizer:
     #    summary = summarizer(text, max_length=150, min_length=30, do_sample=False)
     #    return summary[0]['summary_text']
     
+    def load_groq(self):
+        load_dotenv()
+    
+        GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    
+        client = Groq(
+            api_key=GROQ_API_KEY
+        )
+    
+        return client
+
     def summarize_using_groq(self, text):
         chat_completion = self.client.chat.completions.create(
             messages=[
@@ -75,20 +93,44 @@ class Summarizer:
     
         return chat_completion.choices[0].message.content
     
-    def load_groq(self):
-        load_dotenv()
-    
-        GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    
-        client = Groq(
-            api_key=GROQ_API_KEY
+    def summarize_using_swedishbeagle(self, text):
+        # https://huggingface.co/FredrikBL/SwedishBeagle-dare
+
+        model = "FredrikBL/SwedishBeagle-dare"
+        messages = [
+            {
+                "role": "system", 
+                "content": "You summarize texts that the users sends"
+            }, 
+            {
+                "role": "user", 
+                "content": text
+            }
+        ]
+
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        pipeline = transformers.pipeline(
+            "text-generation",
+            model=model,
+            torch_dtype=torch.float16,
+            device_map="auto",
         )
-    
-        return client
-    
+
+        outputs = pipeline(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+        return outputs[0]["generated_text"]
+
+    def summarize(self, text):
+        if(self.model == "groq"):
+            return self.summarize_using_groq(text)
+        elif(self.model == "SwedishBeagle-dare"):
+            return self.summarize_using_swedishbeagle(text)
 # Streamlit app
 def main():
-    summarizer = Summarizer()
+    # Models:
+    # - groq
+    # - SwedishBeagle-dare
+    summarizer = Summarizer(model="SwedishBeagle-dare")
     summarizer.run_app()
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-from transformers import pipeline
 
-print("hello")
 
-pipleline = pipeline("summarization")
+from transformers import AutoTokenizer
+import transformers
+import torch
+
+model = "FredrikBL/SwedishBeagle-dare"
+messages = [
+            {
+                "role": "system", 
+                "content": "You summarize texts that the users sends"
+            }, 
+            {
+                "role": "user", 
+                "content": "This is a test text."
+            }
+        ]
+tokenizer = AutoTokenizer.from_pretrained(model)
+prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+
+outputs = pipeline(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+print(outputs[0]["generated_text"])
